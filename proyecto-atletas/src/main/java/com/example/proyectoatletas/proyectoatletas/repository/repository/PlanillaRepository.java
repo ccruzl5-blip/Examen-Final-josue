4f964cc56da3533f164ef6ba3687b8c81b86cd2f
ccruzl5-blip <ccruzl5@miumg.edu.gt>
2025-11-08 08:40:06 -0600

    Add files via upload

---
+++ proyecto-atletas/src/main/java/com/example/proyectoatletas/proyectoatletas/repository/repository/PlanillaRepository.java
@@ -0,0 +1,8 @@
+package com.example.proyectoatletas.proyectoatletas.repository.repository;
+
+import com.example.proyectoatletas.proyectoatletas.entity.entity.Planilla;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanillaRepository extends JpaRepository<Planilla, Integer> {
+
+}
